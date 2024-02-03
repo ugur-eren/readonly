2fb69de9efbeb32aaabf401307b51d922015b3b2
Uğur Eren <contact@ugureren.net>
2024-02-03 04:58:01 +0300

    Metamask context hooks

--- packages/site/src/vite-env.d.ts
+++ packages/site/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+import type {MetaMaskInpageProvider} from '@metamask/providers';
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider & {
+      setProvider?: (provider: MetaMaskInpageProvider) => void;
+      detected?: MetaMaskInpageProvider[];
+      providers?: MetaMaskInpageProvider[];
+    };
+  }
+}
